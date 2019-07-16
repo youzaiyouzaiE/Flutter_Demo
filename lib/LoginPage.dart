897f23ce0247d9d68d35e95f2bfd06c13a4a9260
jiahui <zhoujiahui03@meituan.com>
2019-07-16 11:19:59 +0800

    textField 的 数据获取

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -3,78 +3,92 @@ import 'package:flutter_app_login/HomePage.dart';
 
 class LoginPage extends StatefulWidget {
 //  LoginPage({Key key, this.title}) : super(key: key);
-
-//  final String title;
-
   @override
    State<StatefulWidget> createState() => _LoginState();
 }
 
 
 class _LoginState extends State <LoginPage> {
+   double width;
+   double height;
+
+   TextEditingController _accountController = new TextEditingController();
+   TextEditingController _passwordController = new TextEditingController();
+
 
   _pushToHomePage() {
-    Navigator.push(context, new MaterialPageRoute(builder: (context)=> new HomePage()));
+    if(_accountController.text.toString() == "zhoujiahui03" && _passwordController.text.toString() == "123456") {
+//        Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("登录成功")));
+        Navigator.push(context, new MaterialPageRoute(builder: (context)=> new HomePage()));
+    } else {
+//      Scaffold.of(context).showSnackBar(new SnackBar(content: new Text("用户名密码有误")));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
 //    Color color = Theme.of(context).primaryColor;
     final size = MediaQuery.of(context).size;
-    final width = size.width;
-//    final height = size.height;
+    this.width = size.width;
+    this.height = size.height;
 
     return Scaffold(
       appBar: AppBar(
         title: Text("登录login"),
       ),
-      body: Container(
-        padding:new EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
-        color: Colors.green,
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        reverse: false,
+//        padding: EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+        physics: BouncingScrollPhysics(),
+        child: Container(
+          height: height,
+          padding:new EdgeInsets.fromLTRB(20.0, 30.0, 20.0, 20.0),
+          color: Colors.green,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            children: [
+            children: <Widget> [
               Container (
                   padding: EdgeInsets.only(bottom: 10.0),
                   height: 80,
                   color: Colors.blue[400],
-                  child:_inputItem(Icons.supervisor_account,'账号:')
+                  child:_inputItem(Icons.supervisor_account,'账号:', false, _accountController)
               ),
               Container (
                   padding: EdgeInsets.only(top: 20.0),
                   height: 90,
                   color: Colors.orange,
-                  child:_inputItem(Icons.https,'密码:')
+                  child:_inputItem(Icons.https, '密码:', true, _passwordController)
               ),
               Container (
-                  padding: EdgeInsets.fromLTRB(20, 80, 20, 30),
-                    width: width - 20 *2,
-//                  child: new Container(
-                    color: Colors.brown[300],
-                    child: new RaisedButton (
-                      padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                      shape: const RoundedRectangleBorder(
-                        side: BorderSide(width: 1,color: Colors.blueGrey),
-                        borderRadius: BorderRadius.all(Radius.circular(5))
-                      ),
-                      color: Colors.white,
-                      splashColor: Colors.green,
-                      onPressed: _pushToHomePage,
-                      child: Text("登 录",
-                        style: new TextStyle(
-                          fontSize: 24,
-                        ),
-                      ),
+                padding: EdgeInsets.fromLTRB(20, 80, 20, 30),
+                width: width - 20 *2,
+                color: Colors.brown[300],
+                child: new RaisedButton (
+                  padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  shape: const RoundedRectangleBorder(
+                      side: BorderSide(width: 1,color: Colors.blueGrey),
+                      borderRadius: BorderRadius.all(Radius.circular(5))
+                  ),
+                  color: Colors.white,
+                  splashColor: Colors.green,
+                  onPressed: _pushToHomePage,
+                  child: Text("登 录",
+                    style: new TextStyle(
+                      fontSize: 24,
                     ),
-//                  ),
+                  ),
+                ),
               ),
             ],
           ),
+        ),
       ),
     );
   }
 
-  Widget _inputItem(IconData icon, String label) {
+  Widget _inputItem(IconData icon, String label, bool secure, TextEditingController controller) {
+    double textFieldW = this.width - 140;
     return new Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children:[
@@ -92,10 +106,12 @@ class _LoginState extends State <LoginPage> {
         ),
         new ConstrainedBox (
           constraints: BoxConstraints(
-            maxWidth: 250,
+            maxWidth: textFieldW,
           ),
           child: new TextField(
-            
+            controller: controller,
+//            onChanged: ,
+            obscureText: secure,
             style: TextStyle(
               fontSize: 24,
               color: Colors.white
